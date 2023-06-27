9f9864b98c45df0d2920d18019318695309c29c6
SRINU <vasu_anand01@yahoo.co.in>
2023-06-27 17:17:51 +0530

    catalogue

---
+++ catalouge.sh
@@ -0,0 +1,39 @@
+echo -e "\e[33m  dowload NodeJS file  \e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[33m  Install NodeJS  \e[0m"
+yum install nodejs -y
+
+echo -e "\e[33m  Adding a Roboshop user \e[0m"
+useradd roboshop
+
+echo -e "\e[33m Creating a app Directory  \e[0m"
+rm -rf /app
+mkdir /app 
+
+echo -e "\e[33m Dowloading and unzip the Catalogue Contend \e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+
+echo -e "\e[33m Install  nodes  \e[0m"
+cd /app 
+npm install 
+
+echo -e "\e[33m Creating the Catalogue Services \e[0m"
+cp /home/centos/roboshop-shell2/catalouge.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[33m SystemD run  \e[0m"
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl restart catalogue
+
+echo -e "\e[33m mongodb repo file  \e[0m"
+cp /home/centos/roboshop-shell2/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+
+
+echo -e "\e[33m install mongodb \e[0m"
+yum install mongodb-org -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m  Loading the catalouge \e[0m"
+mongo --host mongodb-dev.devopssessions.store </app/schema/catalogue.js
---
+++ mongodb.sh
@@ -0,0 +1,14 @@
+echo -e "\e[33m mongodb repo file  \e[0m"
+cp /home/centos/roboshop-shell2/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33m install mongodb \e[0m"
+yum install mongodb-org -y &>>/tmp/roboshop.log
+
+
+echo -e "\e[33m Update MongoDB Listen Address \e[0m"
+sed -i 's//127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
+
+echo -e "\e[33m SystemD start  \e[0m"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod
+
