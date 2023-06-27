d807fabf3bd4f2b34b25e5e02b0d7b80a4c8e3d9
SRINU <vasu_anand01@yahoo.co.in>
2023-06-27 15:50:34 +0530

    1change

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,20 @@
 echo -e "\e[33m install niginx \e[0m"
-yum install nginx -y &>>/temp/roboshop.log 
+yum install nginx -y &>>/tmp/roboshop.log
 
-echo -e "\e[33m install niginx \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/temp/roboshop.log
 
-echo -e "\e[33m install niginx \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/temp/roboshop.log
+echo -e "\e[33m removi content \e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
-echo -e "\e[33m install niginx \e[0m"
-cd /usr/share/nginx/html &>>/temp/roboshop.log
-unzip /tmp/frontend.zip &>>/temp/roboshop.log
+echo -e "\e[33m uploading the content \e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+
+echo -e "\e[33m unzip the frontenf content \e[0m"
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 #echo -e "\e[33m install niginx \e[0m"
 #cp 
 
-echo -e "\e[33m install niginx \e[0m"
-systemctl enable nginx &>>/temp/roboshop.log
-systemctl restart nginx &>>/temp/roboshop.log
+echo -e "\e[33m systemd start \e[0m"
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
