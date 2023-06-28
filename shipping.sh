621ee7c5a688fae16d39d9a0dd1935ff67c81e12
SRINU <vasu_anand01@yahoo.co.in>
2023-06-28 16:05:55 +0530

    a

--- shipping.sh
+++ shipping.sh
@@ -17,8 +17,7 @@ echo -e "\e[33m dowload Maven Dependencies  \e[0m"
 mvn clean package &>>/tmp/roboshop.log
 mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 
-echo -e "\e[33m Copy the shipping service  \e[0m"
-cp /home/centos/roboshop-shell2/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
+
 
 
 echo -e "\e[33m Installing the MYSQL \e[0m"
@@ -26,6 +25,9 @@ yum install mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Load Schema  \e[0m"
 mysql -h mysql-dev.devopssessions.store -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
+
+echo -e "\e[33m Copy the shipping service  \e[0m"
+cp /home/centos/roboshop-shell2/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
 
 echo -e "\e[33m start the SystemD \e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
